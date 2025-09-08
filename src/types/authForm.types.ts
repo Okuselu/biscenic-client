c75282544980b5c31d72bd846e1a4ac566d3bfa0
Ayotunde Obasa <55897771+ayotunde-codes@users.noreply.github.com>
2025-09-08 11:30:25 +0100

    start up code

--- src/types/authForm.types.ts
+++
@@ -1,6 +0,0 @@
-//src/types/authForm.types.ts
-export interface AuthFormProps {
-    title: string;
-    onSubmit: (e: React.FormEvent) => void;
-    children: React.ReactNode;
-  }
--- src/types/notification.types.ts
+++
@@ -1,5 +0,0 @@
-//src/types/notification.types.ts
-export interface NotificationProps {
-  message: string;
-  type: "success" | "error" | "";
-}
